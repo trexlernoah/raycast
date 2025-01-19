e8d21ce3bae6abe79e60df484ce04e446090745d
Noah Trexler <trexlernoah@gmail.com>
2025-01-18 19:48:04 -0500

    working window

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-// use raycast::run;
-
-
+use raycast::run;
 
 fn main() {
-    // std::env::set_var("WINIT_UNIX_BACKEND", "x11");
-    // env_logger::init(); // Necessary for logging within WGPU
-    //let event_loop = EventLoop::new().unwrap(); // Loop provided by winit for handling window events
-   // let window = WindowBuilder::new().build(&event_loop).unwrap();
-
-    // Opens the window and starts processing events (although no events are handled yet)
-   // event_loop.run(move |event, control_flow| {});
-    println!("hello world");
+    env_logger::init();
+    pollster::block_on(run());
 }
